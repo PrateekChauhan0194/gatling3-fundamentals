cb5418dee612caeb1a1c4fd11d89e9eb6921b30d
Prateek Chauhan <68464629+PrateekChauhan0194@users.noreply.github.com>
2022-09-06 16:01:50 +1000

    Added the comments to CsvFeeder class for better understanding

--- src/test/scala/MySimulations/CsvFeeder.scala
+++ src/test/scala/MySimulations/CsvFeeder.scala
@@ -14,14 +14,21 @@ class CsvFeeder extends Simulation {
   val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
     .acceptHeader("application/json")
 
+  /**
+   * Creating a feeder instance.
+   * This will,
+   * 1. Pull the data from the specified csv
+   * 2. Store the values in the session storage for it to be used in the test scenario
+   */
   val csvFeeder = csv("data/gameCsvFile.csv").circular
 
   def getSpecificGame() = {
     repeat(5) {
-      feed(csvFeeder).exec(
-          http("Get specific game: #{gameName}")
+      feed(csvFeeder) // To feed the values from csv
+        .exec(
+          http("Get specific game: #{gameName}")  // Using the csv feed value from the session
             .get("/videogame/#{gameId}")
-            .check(jsonPath("$.name").is("#{gameName}"))
+            .check(jsonPath("$.name").is("#{gameName}"))  // Using the csv feed value from the session
             .check(status.is(200))
         ).pause(1)
     }
